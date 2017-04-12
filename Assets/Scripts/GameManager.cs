05b097ef16fc8d325b531d0e2b3c0139ab341295
Ben Costrell <bencostrell@gmail.com>
2017-04-12 13:17:52 -0400

    initial unity files

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public Player[] players;
+    public Vector3[] playerSpawns;
+
+	void Awake()
+    {
+        InitializeServices();
+    }
+    
+    // Use this for initialization
+	void Start () {
+        InitializePlayers();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Reset"))
+        {
+            Reset();
+        }
+	}
+
+    void InitializeServices()
+    {
+        Services.GameManager = this;
+        Services.EventManager = new EventManager();
+        Services.TaskManager = new TaskManager();
+        Services.Prefabs = Resources.Load<PrefabDB>("Prefabs/Prefabs");
+    }
+
+    void InitializePlayers()
+    {
+        players = new Player[2]
+        {
+            InitializePlayer(1),
+            InitializePlayer(2)
+        };
+    }
+
+    Player InitializePlayer(int playerNum)
+    {
+        GameObject playerObj = Instantiate(Services.Prefabs.Player, playerSpawns[playerNum - 1], Quaternion.identity);
+        Player player = playerObj.GetComponent<Player>();
+        player.playerNum = playerNum;
+        return player;
+    }
+
+    void Reset()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}

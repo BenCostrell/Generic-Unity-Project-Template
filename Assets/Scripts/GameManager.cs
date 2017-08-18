4d82a6401866e2d80a34b63d68996a862b47047f
Unknown <bencostrell@gmail.com>
2017-08-18 10:54:42 -0400

    task updates, square sprite

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,33 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
     public GameObject sceneRoot;
 
     public int numPlayers;
-    [HideInInspector]
-    public List<Player> players;
+    public List<Player> players { get; private set; }
     public Vector3[] playerSpawns;
 
-	void Awake()
+    void Awake()
     {
         InitializeServices();
     }
-    
+
     // Use this for initialization
-	void Start () {
+    void Start()
+    {
         InitializePlayers();
         Services.EventManager.Register<Reset>(Reset);
         Services.SceneStackManager.PushScene<TitleScreen>();
-        //Services.TaskManager.AddTask(TestBranch(1));
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         Services.InputManager.GetInput();
         Services.TaskManager.Update();
-	}
+    }
 
     void InitializeServices()
     {
@@ -60,32 +61,4 @@ public class GameManager : MonoBehaviour {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
-    //TaskTree TestBranch(int branchNum)
-    //{
-    //    TaskTree testBranch = new TaskTree(
-    //        new DebugTask("starting test branch " + branchNum),
-    //        new TaskTree(new DebugTask("immediate test branch " + branchNum + " child 1")),
-    //        new TaskTree(new DebugTask("immediate test branch " + branchNum + " child 2")),
-    //        new TaskTree(new DebugTask("immediate test branch " + branchNum + " child 3"), 
-    //            new TaskTree(new WaitTask(1f), 
-    //                new TaskTree(new DebugTask("delayed test branch " + branchNum  + " child 3 subchild"))))
-    //        );
-    //    return testBranch;
-    //}
 }
-
-//public class DebugTask : Task
-//{
-//    public string debugMessage;
-
-//    public DebugTask(string _debugMessage)
-//    {
-//        debugMessage = _debugMessage;
-//    }
-
-//    protected override void Init()
-//    {
-//        Debug.Log(debugMessage + " at time " + Time.time);
-//        SetStatus(TaskStatus.Success);
-//    }
-//}
--- Assets/Scripts/Util/Tasks/Task.cs
+++ Assets/Scripts/Util/Tasks/Task.cs
@@ -143,6 +143,24 @@ public class Task {
         Debug.Assert(!taskTree.root.IsAttached);
         nextTasks = new List<Task>(){ taskTree.DistributedTree() };
     }
+
+    public Task Then(TaskQueue taskQueue)
+    {
+        foreach (Task task in taskQueue.tasks)
+        {
+            Debug.Assert(!task.IsAttached);
+        }
+
+        nextTasks = new List<Task>();
+
+        nextTasks.Add(taskQueue.tasks[0]);
+
+        for (int i = 1; i < taskQueue.tasks.Count; i++)
+        {
+            taskQueue.tasks[i - 1].nextTasks.Add(taskQueue.tasks[i]);
+        }
+        return taskQueue.tasks[taskQueue.tasks.Count - 1];
+    }
 }
 
 public class TaskTree
@@ -157,12 +175,102 @@ public class TaskTree
         foreach (TaskTree child in _children) children.Add(child);
     }
 
+    public TaskTree AddChild(Task child)
+    {
+        return AddChild(new TaskTree(child));
+    }
+
+    public TaskTree AddChild(TaskTree child)
+    {
+        children.Add(child);
+        return this;
+    }
+
+    public TaskTree AddChildren(List<TaskTree> children_)
+    {
+        children.AddRange(children_);
+        return this;
+    }
+
+    public TaskTree AddChildren(List<Task> children_)
+    {
+        for (int i = 0; i < children_.Count; i++)
+        {
+            children.Add(new TaskTree(children_[i]));
+        }
+        return this;
+    }
+
     public Task DistributedTree()
     {
         List<Task> childrenDistributed = new List<Task>();
         if (children.Count > 0) foreach (TaskTree child in children) childrenDistributed.Add(child.DistributedTree());
         root.Then(childrenDistributed);
         return root;
+    }
+
+    public TaskTree Then(TaskTree nextTree)
+    {
+        if (children.Count > 0)
+        {
+            return children[0].Then(nextTree);
+        }
+        else
+        {
+            return AddChild(nextTree);
+        }
+    }
+
+    public TaskTree Then(Task nextTask)
+    {
+        return Then(new TaskTree(nextTask));
+    }
+
+    public int Depth()
+    {
+        if(children.Count == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return 1 + children[0].Depth();
+        }
+    }
+}
+
+public class EmptyTask : Task
+{
+    protected override void Init()
+    {
+        SetStatus(TaskStatus.Success);
+    }
+}
+
+public class TaskQueue
+{
+    public List<Task> tasks;
+
+    public TaskQueue(List<Task> taskList)
+    {
+        tasks = taskList;
+    }
+
+    public TaskQueue()
+    {
+        tasks = new List<Task>();
+    }
+
+    public TaskQueue Then(TaskQueue taskQueue)
+    {
+        tasks.AddRange(taskQueue.tasks);
+        return this;
+    }
+
+    public TaskQueue Add(Task task)
+    {
+        tasks.Add(task);
+        return this;
     }
 }
 
